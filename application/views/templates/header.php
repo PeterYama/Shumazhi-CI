011b3f1b1eeac3e45e4558e6d5c61a944062782a
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-02-04 11:41:21 +1100

    changes on header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,6 +10,27 @@
       <!--Let browser know website is optimized for mobile-->
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
+	<body>
+	
+	<ul id="dropdown1" class="dropdown-content">
+		<li><a href="#!">one</a></li>
+		<li><a href="#!">two</a></li>
+		<li class="divider"></li>
+		<li><a href="#!">three</a></li>
+	</ul>
+	
+	<!-- NavigationBar -->
+		<nav>
+			<div class="nav-wrapper #81c784 green lighten-2">
+				<a href="#!" class="brand-logo"></a>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="<?php echo base_url(); ?>home">Home</a></li>
+						<li><a href="<?php echo base_url(); ?>login">Login</a></li>
+						<li><a href="<?php echo base_url(); ?>product">Products</a></li>
+						<li><a href="<?php echo base_url(); ?>product_registration">product registration</a></li>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
 <body>
 	<ul id="dropdown1" class="dropdown-content">
 		<li><a href="#!">one</a></li>
@@ -23,6 +44,8 @@
 			<div class="nav-wrapper #81c784 green lighten-2">
 				<a href="#!" class="brand-logo"></a>
 					<ul class="right hide-on-med-and-down">
+						<li><a href="<?php echo base_url(); ?>/product">Products</a></li>
+						<li><a href="badges.html">About</a></li>
 						<li><a href="<?php echo base_url(); ?>home">Home</a></li>
 						<li><a href="<?php echo base_url(); ?>login">Login</a></li>
 						<li><a href="<?php echo base_url(); ?>product">Products</a></li>
