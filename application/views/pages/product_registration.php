dbfd1ae74b2464de38702d8adda69d862887c7fa
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-02-12 14:05:32 +1100

    minor changes

--- application/views/pages/product_registration.php
+++ application/views/pages/product_registration.php
@@ -30,41 +30,6 @@
 
 
 				<div class="col s12" style="margin-top:50px;">
-<<<<<<< HEAD
-					<form action="#">
-						<div class="file-field input-field">
-							<div class="btn-small right">
-								<span>Select File </span>
-								<input name="product_instructions" type="file" multiple>
-							</div>
-							<div class="file-path-wrapper">
-								<input class="file-path validate" type="text">
-							</div>
-						</div>
-					</form>
-				</div>
-
-
-
-				<div class="col s12" style="margin-top:50px;">
-					<form action="#">
-						<div class="file-field input-field">
-							<div class="btn-small right">
-								<span>Select Images</span>
-								<input name="product_image" type="file" multiple>
-							</div>
-							<div class="file-path-wrapper">
-								<input class="file-path validate" type="text">
-							</div>
-						</div>
-					</form>
-				</div>
-				<input class="btn btn-success" name="register" type="submit" class="btn btn-register" value="Confirm" />
-			</div>
-		</form>
-	</div>
-	<div>
-=======
 					<!-- <form action="#" > -->
 						<div class="file-field input-field">
 							<div class="btn-small right">
@@ -104,4 +69,3 @@
 					<div>
 
 						
->>>>>>> origin/SujataTemp
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Shumazhi</title>
-		 <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-      <!--Let browser know website is optimized for mobile-->
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	</head>
+<head>
+	<title>Shumazhi</title>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<style>
+		body { background-image: 
+			/* linear-gradient(217deg, rgba(0,50,200,8), rgba(0,100,100,0) 70.71%),
+            linear-gradient(127deg, rgba(0,100,100,.8), rgba(200,100,0,0) 70.71%),
+            linear-gradient(336deg, rgba(0,200,0,.8), rgba(0,150,200,0) 70.71%), */
+		url('http://localhost/Shumazhi-CI/assets/images/Fuji.jpg');
+		   }
+		.navbar-default {
+			background-color: #29545b;
+			border-color: #E7E7E7;
+		}
+	</style>
+</head>
+
 <body>
-	<ul id="dropdown1" class="dropdown-content">
-		<li><a href="#!">one</a></li>
-		<li><a href="#!">two</a></li>
-		<li class="divider"></li>
-		<li><a href="#!">three</a></li>
-	</ul>
-	
-	<!-- NavigationBar -->
+		<!-- NavigationBar -->
 		<nav>
-			<div class="nav-wrapper #81c784 green lighten-2">
+			<div class="nav-wrapper navbar-default">
 				<a href="#!" class="brand-logo"></a>
-					<ul class="right hide-on-med-and-down">
-						<li><a href="<?php echo base_url(); ?>home">Home</a></li>
-						<li><a href="<?php echo base_url(); ?>login">Login</a></li>
-						<li><a href="<?php echo base_url(); ?>product">Products</a></li>
-						<!-- <li><a href="<?php echo base_url(); ?>Product/viewProducts">Products</a></li> -->
-
-						<li><a href="<?php echo base_url(); ?>product_registration">product registration</a></li>
-						<!-- Dropdown Trigger -->
-						<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
-					</ul>
+				<ul class="right hide-on-med-and-down">
+					<li><a href="badges.html">About</a></li>
+					<li><a href="<?php echo base_url(); ?>home">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>login">Login</a></li>
+					<li><a href="<?php echo base_url(); ?>product">Products</a></li>
+					<li><a href="<?php echo base_url(); ?>product_registration">product registration</a></li>
+					<!-- Dropdown Trigger -->
+					<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
+				</ul>
 			</div>
 		</nav>
-
-
