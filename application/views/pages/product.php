a03e11af384e740438dfcbf38343e8c5fe372962
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-02-13 20:36:32 +1100

    removed table

--- application/views/pages/product.php
+++ application/views/pages/product.php
@@ -2,17 +2,15 @@
 
   <div class="row" style="padding:10px;">
     <div class="col s6" style="margin-top: 10px;">Product Page</div>
-    <form action="" class="browser-default right">
-
-      <input id="search-input" placeholder="Search" type="text" class="browser-default search-field" name="q" value="" autocomplete="off" aria-label="Search box">
-      <label for="search-input"><i class="material-icons search-icon">search</i></label>
-      <div class="search-popup">
-        <div class="search-content">
-        </div>
-      </div>
-
-    </form>
-  </div>
+        <form action="" class="browser-default right">
+          <input id="search-input" placeholder="Search" type="text" class="browser-default search-field" name="q" value="" autocomplete="off" aria-label="Search box">
+          <label for="search-input"><i class="material-icons search-icon">search</i></label>
+          <div class="search-popup">
+            <div class="search-content">
+            </div>
+          </div>
+        </form>
+    </div>
   <div class="row">
     <div class="col s12 ">
       <div class="card hoverable blue-grey darken-1">
@@ -49,31 +47,6 @@
         <div class="card-action">
           <a href="#" class="dark-text text-darken-2">Edit</a>
           <a href="#" class="dark-text text-darken-2">Delete</a>
-
-          <!--  <table id = "productList" class = "table" border="1" border-collapse="collapse">
-          <thead>
-           <tr>
-            <th role="button">Product</th>
-            <th role="button">Description</th>
-            <th role="button">Brand</th>
-            <th role="button">Image</th>
-            <th role="button">Instructions</th>
-            <th role="button">QR</th>
-          </tr>
-        </thead>
-        <tbody>
-         <?php
-          // if ($products) {
-          // foreach ($products as $product){
-          ?>
-            <tr>
-              <td><?php echo $product->product_name ?></td>
-              <td><?php echo $product->product_description ?></td>
-              <td><?php echo $product->product_image ?></td>
-              <td><?php echo $product->product_instructions ?></td>
-              <td><?php echo $product->brand_name ?></td>
-      </tbody>
-    </table> -->
         </div>
 
       </div>
